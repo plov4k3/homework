45c68e421cadf6b0e74d2b38b41e56d688cfeb72
plov4k3 <plov4k3@mail.ru>
2025-04-06 18:28:34 +0300

    Add files via upload

---
+++ Homework JS/main.js
@@ -0,0 +1,93 @@
+const Admin = {
+    name: "Danila",
+    age: 18,
+    isAdmin: true,
+    sayHello(name) {
+        console.log(`Hello ${name}`)
+    }
+}
+
+console.log(Admin)
+Admin.sayHello("Aroken")
+
+// =============================================================
+
+
+
+
+const users = [
+    {
+    name:"Ivan",
+        age:21,
+        isAdmin:false
+    },
+    {
+    name:"Jhon",
+        age:23,
+        isAdmin:false
+    },
+    {
+        name: "Kris",
+        age: 20,
+        isAdmin: true
+    },
+    {
+        name: "Mark",
+        age: 23,
+        isAdmin: false
+    },
+    {
+        name: "Tom",
+        age: 30,
+        isAdmin: true
+    },
+    {
+        name: "Danila",
+        age: 18,
+        isAdmin: true
+    },
+    {
+        name: "july",
+        age: 25,
+        isAdmin: false
+    }
+]
+
+
+
+for(let i = 0;i < users.length;i++){
+    console.log(users[i])
+}
+
+
+
+// =============================================
+
+
+
+
+const noAdmin = [
+    {
+        name:"Ivan",
+            age:21,
+            isAdmin:false
+        },
+        {
+        name:"Jhon",
+            age:23,
+            isAdmin:false
+        },
+        {
+            name: "Mark",
+            age: 23,
+            isAdmin: false
+        },
+        {
+            name: "july",
+            age: 25,
+            isAdmin: false
+        }
+]
+
+
+console.log(noAdmin)
