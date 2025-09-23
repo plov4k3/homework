2f70729b98ee44c408dc17c8fb837955e3fa7248
plov4k3 <plov4k3@mail.ru>
2025-09-24 00:54:33 +0300

    Add files via upload

---
+++ projekt-12/main.js
@@ -0,0 +1,59 @@
+const user = {
+    Danila: {
+        age:19,
+        height:172,
+        sayHello(name) {
+            console.log(`Hello ${name}`)
+        }
+    }
+}
+
+console.log(user.Danila)
+user.Danila.sayHello('Danila')
+// ===========================================
+
+
+const  simple  = 4
+const users = [
+    {
+        name: 'Dima',
+        age: 16,
+        isAdmin: false
+    },
+    {
+        name: 'Roma',
+        age: 28,
+        isAdmin: true
+    },
+    {
+        name: 'Vika',
+        age: 20,
+        isAdmin: false
+    },
+    {
+        name: 'Sonya',
+        age: 35,
+        isAdmin: true
+    },
+    {
+        name: 'Max',
+        age: 30,
+        isAdmin: true
+    },
+    {
+        name: 'Ivan',
+        age: 21,
+        isAdmin: false
+    },
+    {
+        name: 'Misha',
+        age: 24,
+        isAdmin: true
+    },
+]
+
+console.log(users)
+console.log(simple)
+
+
+
